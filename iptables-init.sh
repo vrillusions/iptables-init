2a57ee9da4b82b8941bb76bb6b5059bdd7e4d8a8
Todd Eddy <vr@vrillusions.com>
2015-01-08 00:23:06 -0500

    Clean up documentation and fix a couple minor bugs
    * Add CONTRIBUTING with a few things people that submit code
      contributions should keep in mind.
    * Clarify some things in README
    * Clarify things in iptables-init.sh
    * FIX: The ssh limiter was actually limiting to 6 attempts every 30
      seconds. Changed to what's mentioned in README which is 8 attempts
      every minute
    * Change ping limit to 10/second instead of 60/minute. This way one
      person pinging the server means anyone else pinging won't get a reply.
      This can affect commands like mtr

--- iptables-init.sh
+++ iptables-init.sh
@@ -14,17 +14,22 @@
 # While the overhead is small keep in mind that iptables rules are evaluated
 # from the top down and the first one the matches is the action that is taken.
 # If a certain port is really popular you may want to consider moving it further
-# up
+# up.
+#
+# Licensed under a dual permissive license of Unlicense and MIT. See LICENSE.txt
 #
 
+# -- Exit on errors (-e) and Exit if try to use an unset variable (-u) --
 set -e
 set -u
 
+# -- Only root can set iptables rules --
 if [[ $EUID -ne 0 ]]; then
     printf "%s" "Must be run as root" >&2
     exit 1
 fi
 
+# -- Setup our env variables (need this or will exit if not set) --
 readonly VERBOSE="${VERBOSE:-false}"
 readonly DEBUG="${DEBUG:-false}"
 
@@ -32,11 +37,18 @@ if [[ "${DEBUG}" == "true" ]]; then
     set -x
 fi
 
-# Logging Functions
+
+# == Helper functions ==
+
+# -- print line to screen with timestamp --
 # Usage: log "What to log"
 log () {
+    # Uncomment this line to log messages to syslog
+    #logger -s -t "${script_name}" -- "$*"
     printf "%b\n" "$(date +"%Y-%m-%dT%H:%M:%S%z") $*"
 }
+
+# -- only print line if verbose is true --
 # Usage: verbose "What to log if VERBOSE is true"
 verbose () {
     if [[ "${VERBOSE}" == "true" ]]; then
@@ -45,77 +57,93 @@ verbose () {
 }
 
 
+# -- setup iptables_cmd variable --
+# Use this to change command that is run for entire script
 iptables_cmd=$(which iptables) || eval "echo 'Could not find iptables'; exit 1"
 
 
 verbose "Setting iptables rules"
 verbose "Clearing current rules"
-# Start from scratch
 ${iptables_cmd} -F
 ${iptables_cmd} -Z
 ${iptables_cmd} -X
 
 
-# LIMIT chain, used as endpoint of limited connections
+# -- create LIMIT chain for throttled connections --
 verbose "Creating LIMIT chain"
 ${iptables_cmd} -N LIMIT
 ${iptables_cmd} -A LIMIT -m limit --limit 3/min -j LOG --log-prefix "[LIMIT BLOCK] "
 ${iptables_cmd} -A LIMIT -j REJECT --reject-with icmp-port-unreachable
 
 
+# -- start setting specific iptables rules --
 verbose "Creating main iptables rules"
-# Allow existing connections
-# For OpenVZ servers:
-#   - need to modprobe xt_tcpudp, ip_conntrack, and xt_state
+
+# -- allow established connections --
+# ## IMPORTANT: For OpenVZ servers ##
+#   - need to modprobe xt_tcpudp, ip_conntrack, and xt_state. To do this add
+#     those three names to /etc/modules and restart.
 #   - uncomment the state line and comment out the conntrack line if it doesn't
 #     work.
 #${iptables_cmd} -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 ${iptables_cmd} -I INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 
-# allow local interface
+# -- allow all traffic on local interface --
 ${iptables_cmd} -A INPUT -i lo -j ACCEPT
 
 
-# ok icmp codes
+# -- ICMP types that should always be allowed --
+# Messages about a host not being reachable
 ${iptables_cmd} -A INPUT -p icmp --icmp-type destination-unreachable -j ACCEPT
+# Messages to slow down how fast your sending data
 ${iptables_cmd} -A INPUT -p icmp --icmp-type source-quench -j ACCEPT
+# Time-to-live is exceeded
 ${iptables_cmd} -A INPUT -p icmp --icmp-type time-exceeded -j ACCEPT
+# Something about the packet that the server sent is wrong
 ${iptables_cmd} -A INPUT -p icmp --icmp-type parameter-problem -j ACCEPT
-# can lower this if you're paranoid but 1 ping a second is fine.  If someone is
-# trying to ping flood they're going to attempt thousands a second. Can also
-# comment out if you don't want to respond to pings but that's not recommended
-# for servers
+# The ping command defaults to one ping a second. Use 10 per second or else if
+# just two people are pinging a server it won't respond or be really
+# intermittent.  Ping flood attacks are thousands a second so this is still
+# a lot better than nothing.  Deleting this line will block pings but that isn't
+# recommended on servers.
 ${iptables_cmd} -A INPUT -p icmp --icmp-type echo-request -j ACCEPT \
-    -m limit --limit 60/minute
+    -m limit --limit 10/second
 
 
-# limit how fast incoming ssh connections can happen
+# -- limit how fast incoming ssh connections can happen --
+# This sets it to 8 times (value for hitcount) every 60 seconds (value for
+# seconds). Feel free to adjust those.  This is per source ip meaning if some
+# botnet is flooding ssh server you should still be able to ssh from your
+# location.
 ${iptables_cmd} -A INPUT -p tcp -m tcp --dport 22 \
     -m state --state NEW \
     -m recent --set --name SSHLIMIT --rsource
 ${iptables_cmd} -A INPUT -p tcp -m tcp --dport 22 \
     -m state --state NEW \
-    -m recent --update --seconds 30 --hitcount 6 --name SSHLIMIT --rsource -j LIMIT
+    -m recent --update --seconds 60 --hitcount 8 --name SSHLIMIT --rsource -j LIMIT
 ${iptables_cmd} -A INPUT -p tcp -m tcp --dport 22 -j ACCEPT
 
 
-# some ports to allow
-# ssh is handled above but if you don't want limiter
+# -- allow aditional ports --
+# ssh is handled above but if you don't want limiter then comment or remove
+# above and uncomment this line
 #${iptables_cmd} -A INPUT -p tcp --dport 22 -j ACCEPT
 ${iptables_cmd} -A INPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
-# example of just 443
+# example of just 80
 #${iptables_cmd} -A INPUT -p tcp --dport 80 -j ACCEPT
 # example to allow full access from a single ip
 #${iptables_cmd} -A INPUT -s 192.231.162.123/32 -j ACCEPT
 
 
-# log unhandled packets
-# Once you have things setup probably want to comment this out so you're not filling logs
+# -- log unhandled packets (run `dmesg` to see log entries) --
+# Will be logged to kernel, viewable by running `dmesg`. Once you have things
+# setup probably want to comment this out so you're not filling logs
 ${iptables_cmd} -A INPUT -m limit --limit 15/min -j LOG --log-prefix "[UNHANDLED INPUT PKT] "
 
 
-# by default allow outgoing traffic, no incoming
+# -- set preferences for any traffic that does match above rules --
+# allow unhandled outgoing traffic but silently drop unhandled incoming traffic
 ${iptables_cmd} -P INPUT DROP
 ${iptables_cmd} -P FORWARD DROP
 ${iptables_cmd} -P OUTPUT ACCEPT
